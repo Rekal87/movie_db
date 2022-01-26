f641dcf6ed04258dc250f9aef708dbcd3fc45c73
Nnamdi <nnamdi.b@gmail.com>
2022-01-26 23:55:39 +0100

    restarting project without TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React from "react";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div>
-      <div className="container mx-auto px-2">
-        <div className="w-80 bg-gray-100 mx-auto top-10 relative">
-          <label className="text-sm text-gray-600 false" for="email">
-            Email Address
-          </label>
-          <div>
-            <input
-              type="email"
-              name="email"
-              className="border border-gray-300 rounded-sm outline-none transition-colors duration-150 ease-in-out focus:border-blue-400"
-            />
-            <span className="mt-2 flex text-gray-600 text-xs">
-              Please enter your work email address
-            </span>
-          </div>
-        </div>
-        <Button />
-      </div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
